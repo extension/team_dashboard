528531db579aed8ba3377648717f114187272cd6
Ben MacNeill <ben.macneill@gmail.com>
2021-10-29 08:49:08 -0400

    Add method to aggregate submission averages

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,46 @@
 class Survey < ApplicationRecord
   belongs_to :team
   has_many :submissions
+
+  def aggregate_psychological_safety_avg_score
+    scores = 0
+    self.submissions.each do |s|
+      scores += s.individual_psychological_safety_avg_score
+    end
+    return scores.to_d/self.submissions.count
+	end
+
+  def aggregate_dependability_avg_score
+    scores = 0
+    self.submissions.each do |s|
+      scores += s.individual_dependability_avg_score
+    end
+    return scores.to_d/self.submissions.count
+	end
+
+  def aggregate_structure_clarity_avg_score
+    scores = 0
+    self.submissions.each do |s|
+      scores += s.individual_structure_clarity_avg_score
+    end
+    return scores.to_d/self.submissions.count
+	end
+
+  def aggregate_meaning_avg_score
+    scores = 0
+    self.submissions.each do |s|
+      scores += s.individual_meaning_avg_score
+    end
+    return scores.to_d/self.submissions.count
+	end
+
+  def aggregate_impact_avg_score
+    scores = 0
+    self.submissions.each do |s|
+      scores += s.individual_impact_avg_score
+    end
+    return scores.to_d/self.submissions.count
+	end
+
+
 end
