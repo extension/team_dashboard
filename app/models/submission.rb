c8881f6132cf8836122a9c47c980bf9e8214c09d
Ben MacNeill <ben.macneill@gmail.com>
2021-10-29 08:48:37 -0400

    Rename submission methods for clarity

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,43 +3,43 @@ class Submission < ApplicationRecord
   has_many :responses
 
 
-  def psychological_safety_avg_score
-		@psychological_safety_avg_score = 0
+  def individual_psychological_safety_avg_score
+		@score = 0
     (1..3).each do |i|
-      @psychological_safety_avg_score += self.responses.where(question_number: i).last.score
+      @score += self.responses.where(question_number: i).last.score
     end
-    return @psychological_safety_avg_score.to_d/3
+    return @score.to_d/3
 	end
 
-  def dependability_avg_score
-		@dependability_avg_score = 0
+  def individual_dependability_avg_score
+		@score = 0
     (4..6).each do |i|
-      @dependability_avg_score += self.responses.where(question_number: i).last.score
+      @score += self.responses.where(question_number: i).last.score
     end
-    return @dependability_avg_score.to_d/3
+    return @score.to_d/3
 	end
 
-  def structure_clarity_avg_score
-		@structure_clarity_avg_score = 0
+  def individual_structure_clarity_avg_score
+		@score = 0
     (7..9).each do |i|
-      @structure_clarity_avg_score += self.responses.where(question_number: i).last.score
+      @score += self.responses.where(question_number: i).last.score
     end
-    return @structure_clarity_avg_score.to_d/3
+    return @score.to_d/3
 	end
 
-  def meaning_avg_score
-		@meaning_avg_score = 0
+  def individual_meaning_avg_score
+		@score = 0
     (10..12).each do |i|
-      @meaning_avg_score += self.responses.where(question_number: i).last.score
+      @score += self.responses.where(question_number: i).last.score
     end
-    return @meaning_avg_score.to_d/3
+    return @score.to_d/3
 	end
 
-  def impact_avg_score
-		@impact_avg_score = 0
+  def individual_impact_avg_score
+		@score = 0
     (13..15).each do |i|
-      @impact_avg_score += self.responses.where(question_number: i).last.score
+      @score += self.responses.where(question_number: i).last.score
     end
-    return @impact_avg_score.to_d/3
+    return @score.to_d/3
 	end
 end
