650343aaf70af2ba0111bbb2425929e0d8c41b90
Mark Locklear <marklocklear@gmail.com>
2021-11-09 09:54:40 -0500

    added google auth

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_google(email:, full_name:, uid:, avatar_url:)
+    return nil unless email =~ /@extension.org\z/
+    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+  end
+end
