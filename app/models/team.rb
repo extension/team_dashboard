070ed874e8011f3ac3f03aa78ab5c2080d33797f
Ben MacNeill <ben.macneill@gmail.com>
2021-11-30 16:37:44 -0500

    Create a deterministic hash from the team name and use for URL

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,11 @@
 class Team < ApplicationRecord
 	has_many :surveys
+	extend FriendlyId
+	friendly_id :slug, use: :slugged
+	before_validation :set_hashid, prepend: true, if: Proc.new{ |team| team.slug.nil? }
+
+	private
+		def set_hashid
+			self.slug = (Digest::SHA256.base64digest self.name).gsub(/[\/=+$]/,"")[0,10]
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_140317) do
+ActiveRecord::Schema.define(version: 2021_11_30_163918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
 
   create_table "responses", force: :cascade do |t|
     t.text "question"
@@ -46,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_11_04_140317) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_teams_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
