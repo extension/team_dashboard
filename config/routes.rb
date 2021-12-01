f966069bd3588ada6a8f031f45e94c8abdd2537b
Ben MacNeill <ben.macneill@gmail.com>
2021-12-01 15:31:58 -0500

    Move sign in page (and remove from nav bar)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
+    get 'admin', to: 'users/sessions#new', as: :new_user_session
     # get 'users/sign_in', to: 'users/sessions#new', as: :session
     get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
   end
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :submissions
   resources :surveys
   resources :teams
+  get 'upload', to: 'home#upload'
   root to: "home#index"
 end
