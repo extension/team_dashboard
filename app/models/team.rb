4128e741cd6f19bb1f567b4f514cdf670be1dd8c
Mark Locklear <marklocklear@gmail.com>
2022-05-04 10:26:56 -0400

    add get_team_member_emails function to team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,21 @@
 class Team < ApplicationRecord
 	has_many :surveys
+
 	extend FriendlyId
 	friendly_id :slug, use: :slugged
+
 	before_validation :set_hashid, prepend: true, if: Proc.new{ |team| team.slug.nil? }
+
+	def get_team_member_emails
+		#team > survey > submission > user.email
+		member_emails = []
+		self.surveys.each do |survey|
+			survey.submissions.each do |submission|
+				member_emails << submission.user.email
+			end
+		end
+		return member_emails
+	end
 
 	private
 		def set_hashid
