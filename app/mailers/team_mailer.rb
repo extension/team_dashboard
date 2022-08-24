750f051b186bdfdb1aec7d54907e982ab67cf551
Mark Locklear <marklocklear@gmail.com>
2022-08-24 10:27:24 -0400

    fix team mailer

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -1,10 +1,8 @@
 class TeamMailer < ApplicationMailer
 	#send email when a team leader submits a new team via google form
 	def new_team_email
-		@leader_name = params[:leader_name]
-    @leader_email = params[:leader_email]
-    @team_name = params[:team_name]
-    @number_of_team_members = params[:number_of_team_members]
-		mail(to: @leader_email, subject: "You Team has been created")
+    @leader_email = params[:email]
+    @team_name = params[:team]
+		mail(to: @leader_email, subject: "Your Team has been created")
 	end
 end
